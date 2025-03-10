7cf8be54dfce53f44e4b12a087b0f9fa157c2960
Ryan Maloney <ryan@veto.chat>
2025-03-10 15:03:38 +0000

    feat: implementing microstomp controller

--- microstomp.py
+++ microstomp.py
@@ -1,6 +1,7 @@
 '''
 Contains code to handle the STOMP communication and subscription.
 '''
+import usocket
 
 class Frame:
     '''
@@ -53,3 +54,63 @@ class Frame:
         _ += self.body + '''\x00'''
 
         return _.encode("utf-8")
+
+class MicroSTOMPClient:
+    '''
+    A client for sending and receiving messages to a STOMP server.
+    '''
+    def __init__(self,
+                 host,
+                 port,
+                 client_id,
+                 username,
+                 password,
+                 on_message_callback):
+        '''
+        :params:
+        :host: str - STOMP server or broker address
+        :port: int - STOMP server or broker port
+        :client_id: str - identifier for client connection
+        :username: str -  auth username
+        :password: str - auth password
+        :on_message_callback: function - callback function
+        '''
+        self.cx_socket = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
+        self.cx_host = host
+        self.cx_port = port
+        self.cx_client_id = client_id
+        self.cx_username = username
+        self.cx_password = password
+        self.on_message_callback = on_message_callback
+        self.connected_to_broker = False
+
+    def connect(self):
+        '''
+        Connects to server provided and returns socket
+        '''
+
+        print("(info): beginning connection to server")
+
+        connect_frame = Frame(
+            command = 'CONNECT',
+            headers = {
+                'accept-version':'1.2',
+                'host':self.cx_host,
+                'login':self.cx_username,
+                'passcode':self.cx_password
+            },
+            body=''
+        ).built_frame
+
+        try:
+            self.cx_socket.connect((self.cx_host, self.cx_port))
+        except Exception as e:
+            print('(fatal): cannot connect to specified host - ', e)
+            return 1
+
+        self.cx_socket.send(connect_frame)
+        server_response = self.cx_socket.recv(1024).decode()
+
+        print('(info): server responded to connect with ', server_response)
+
+        self.connected_to_broker = True
