e6b1d0ba759c3e74700173c04c99ed89dd4230f1
Ryan Maloney <maloney.rpa@gmail.com>
2025-03-13 15:31:30 +0000

    feat: added frame parser method and subscribe, listen methods to the STOMPClient

--- microstomp.py
+++ microstomp.py
@@ -1,9 +1,9 @@
 '''
-Contains code to handle the STOMP communication and subscription.
+Written as a patch-in for Stomp.py for Micropython.
 '''
 import usocket
-import random
-
+import utime
+import re
 class Frame:
     '''
     A STOMP frame structure which adheres to
@@ -56,6 +56,47 @@ class Frame:
 
         return _.encode("utf-8")
 
+    @classmethod
+    def parse_frame(cls, frame):
+        '''
+        A class method to take a frame and parse it
+        into a Frame object.
+
+        :params:
+        :frame: bytes representation of frame
+
+        :returns:
+        : : Frame
+        '''
+        frame = str(frame)
+        command_and_headers = None
+        body_content = None
+        parsed_command = None
+        parsed_headers = dict()
+
+        try:
+            command_and_headers = re.findall('^.*\n', frame, re.MULTILINE)
+            body_content = re.search('(?<=\n)(?!.*\n)([^\\x00]*)', frame, re.MULTILINE).group(1)
+            parsed_command = str(command_and_headers[0]).replace('\n', '')
+            command_and_headers.pop(0)
+        except Exception as e:
+            print("(critical): could not parse frame ", e)
+            return False
+
+        for header in command_and_headers:
+            header = re.split('[:]', str(header).rstrip())
+            if header and len(header) > 1:
+                parsed_headers[header[0]] = header[1]
+            else:
+                print('(warn): header could not be parsed', header)
+
+        return cls(
+            command = str(parsed_command),
+            headers = parsed_headers,
+            body = str(body_content)
+        )
+
+
 class MicroSTOMPClient:
     '''
     A client for sending and receiving messages to a STOMP server.
@@ -84,6 +125,7 @@ class MicroSTOMPClient:
         self.cx_password = password
         self.on_message_callback = on_message_callback
         self.connected_to_broker = False
+        self.exponential_backoff_period = 0
 
     def connect(self):
         '''
@@ -137,7 +179,7 @@ class MicroSTOMPClient:
 
         self.cx_socket.send(disconnect_frame)
         disconnect_response = self.cx_socket.recv(1024).decode("utf-8")
-        
+
         print('(info): received response from server ', disconnect_frame)
 
         if 'DISCONNECT' in disconnect_response and str(disconnect_reference) in disconnect_response:
@@ -148,3 +190,49 @@ class MicroSTOMPClient:
             print('(info): could not gracefully disconnect, force closing')
             self.cx_socket.close()
             self.connected_to_broker = False
+
+    def subscribe(self, topic: str, ack: str = 'auto'):
+        '''
+        Send subscribe frame to server.
+        :params:
+        :topic: str - the destination of the topic
+        :ack: str - client acknowledge type, defaults to auto
+
+        :returns:
+        :bool: if successful return True
+        '''
+        if not self.connected_to_broker:
+            print('(error): cannot subscribe when no active cx')
+            return
+
+        print('(info): beginning subscription')
+
+        subscription_frame = Frame(
+            command='SUBSCRIBE',
+            headers = {
+                'id':self.cx_client_id,
+                'destination':topic,
+                'ack':ack
+            },
+            body=''
+        ).built_frame
+        self.cx_socket.send(subscription_frame)
+        subscription_response = self.cx_socket.recv(5120).decode("utf-8")
+        print('(info): received response from topic server', subscription_response)
+
+    def listen_for_messages(self):
+        '''
+        Listens for messages and passes them to the callback function
+        '''
+        if not self.connected_to_broker:
+            print('(error): cannot listen for messages when no active cx')
+        while True:
+            try:
+                received_message = self.cx_socket.recv(5120).decode("utf-8")
+                if received_message:
+                    print("(info): received message")
+                    self.on_message_callback(received_message)
+            except Exception as e:
+                print('(error): exception when listening or receiving, backing off', e)
+                self.exponential_backoff_period *= 2
+                utime.sleep(self.exponential_backoff_period)
