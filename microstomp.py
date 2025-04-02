4ad5b0fb7849ce3abed614fd535a7408a9640ef1
Ryan Maloney <maloney.rpa@gmail.com>
2025-04-02 18:29:46 +0100

    Add Error as valid message type in Frame class (#13)
    * bug: adding in error as valid message type
    
    * rebase: rebase with main
    
    * upd: replacing local testing import statements

--- microstomp.py
+++ microstomp.py
@@ -22,6 +22,7 @@ class Frame:
         :command: must be string of CONNECT, STOMP, SUBSCRIBE, UNSUBSCRIBE, DISCONNECT
         '''
         if command.upper() not in [
+            'ERROR',
             'MESSAGE',
             'CONNECT',
             'STOMP',
